7b8b1fb0df9644d8bf7bf5f6d380aa2cd78aa5ce
ccollino <colin@clevertech.tv>
2026-06-28 16:00:21 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,15 @@
+const C = 'ap-passage-v1';
+const ASSETS = ['./','./index.html','./manifest.webmanifest','./icon-180.png','./icon-192.png','./icon-512.png'];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(C).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(ks => Promise.all(ks.filter(k => k !== C).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const cc = resp.clone(); caches.open(C).then(c => c.put(e.request, cc)); return resp;
+    }).catch(() => caches.match('./index.html')))
+  );
+});
